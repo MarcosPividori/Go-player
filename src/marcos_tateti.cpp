6db8e71daf7cb473dfb1a49e426ce9a0863298d4
Marcos Pividori <marcos.pividori@gmail.com>
2014-10-20 21:11:33 -0300

    update tateti game with new api

--- src/marcos_tateti.cpp
+++ src/marcos_tateti.cpp
@@ -1,7 +1,9 @@
 
 #include "mcts_utils.hpp"
+#include "mcts_uct.hpp"
 #include <iostream>
 #include <cassert>
+#include <thread>
 
 #define MOVE(i,j,player) ((i)*6+(j)*2+(player))
 #define I(m) (m)/6
@@ -10,9 +12,12 @@
 #define PlayerToCell(p) (p==Circle ? CIRCLE : CROSS)
 #define CellToPlayer(p) (p==CIRCLE ? Circle : (p==CROSS ? Cross : assert(0)))
 #define ChangePlayer(p) (p==Circle ? Cross  : Circle)
+#define NUM_THREADS 5
+#define NUM_CYCLES 100000
 
 typedef long DataTateti;
 typedef double ValTateti;
+typedef NodeUCT<ValTateti,DataTateti> Nod;
 
 typedef enum {
     Circle = 0,
@@ -25,7 +30,7 @@ typedef enum {
     CROSS  =  1,
 } CELL;
 
-class StateTateti: public State<ValTateti,DataTateti>{
+class StateTateti: public States<ValTateti,DataTateti>{
     private:
         CELL A[3][3];
     public:
@@ -122,14 +127,16 @@ bool StateTateti::valid_move(DataTateti d)
     return A[I(d)][J(d)]==EMPTY;
 }
 
-ValTateti fun(ValTateti v_nodo,ValTateti v_final,DataTateti dat_nodo)
-{
-    if(v_final == EMPTY)//Tie
-        return v_nodo+0.9999;
-    if(v_final == PlayerToCell(Player(dat_nodo)))
-        return v_nodo+1;
-    return v_nodo;
-}
+struct EvalNode{
+    ValTateti operator()(ValTateti v_nodo,ValTateti v_final,DataTateti dat_nodo)
+    {
+        if(v_final == EMPTY)//Tie
+            return v_nodo+0.9999;
+        if(v_final == PlayerToCell(Player(dat_nodo)))
+            return v_nodo+1;
+        return v_nodo;
+    }
+};
 
 Player insert_player()
 {
@@ -160,17 +167,17 @@ DataTateti insert_mov(Player player,StateTateti *state)
 
 int main()
 {
-    NodeUCT<ValTateti,DataTateti> *nod=new NodeUCT<ValTateti,DataTateti>(0,0,NULL),*next;
+    Nod *nod=new Nod(0,0),*next;
     StateTateti state;
     DataTateti res;
     std::vector<DataTateti> v;
-    
+    std::mutex mutex;
     SelectionUCT<ValTateti,DataTateti> sel(1);
-    ExpansionAllChildren<ValTateti,DataTateti> exp(2,0);
-    SimulationTotallyRandom<ValTateti,DataTateti> sim;
-    RetropropagationSimple<ValTateti,DataTateti> ret(fun);
-    SelectResMostRobust<ValTateti,DataTateti> sel_res;
-    Mcts<ValTateti,DataTateti,NodeUCT<ValTateti,DataTateti>> m(&sel,&exp,&sim,&ret,&sel_res);
+    ExpansionAllChildren<ValTateti,DataTateti,StateTateti,Nod> exp(2,0);
+    SimulationTotallyRandom<ValTateti,DataTateti,StateTateti> sim;
+    RetropropagationSimple<ValTateti,DataTateti,EvalNode> ret;
+    SelectResMostRobust<ValTateti,DataTateti,Nod> sel_res;
+    Mcts<ValTateti,DataTateti,Nod,StateTateti> m(&sel,&exp,&sim,&ret,&sel_res,&mutex);
     
     std::cout<< "TATETI:"<<std::endl;
     Player us_player=insert_player();
@@ -194,7 +201,13 @@ int main()
             nod->show();
             exp.counter=0;
 #endif
-            m.run_cycles(100000,nod,&state);
+
+        std::thread threads[NUM_THREADS];
+        for(int i=0; i<NUM_THREADS; i++)
+            threads[i] = std::thread(&Mcts<ValTateti,DataTateti,Nod,StateTateti>::run_cycles,m,NUM_CYCLES/NUM_THREADS,nod,&state);
+        for(int i=0; i<NUM_THREADS; i++)
+            threads[i].join();
+
 #ifdef DEBUG
             std::cout<<"Expansion counter: "<<exp.counter<<std::endl;
             nod->show();
@@ -212,7 +225,7 @@ int main()
             nod=next;
         }else{
             nod->delete_tree();
-            nod= new NodeUCT<ValTateti,DataTateti>(0,res,NULL);
+            nod= new Nod(0,res);
         }
 
         state.show();    
